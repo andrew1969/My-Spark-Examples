bdf01ca46a9ef5867513b09181992c03684fe452
andrew1969 <acarbone@sysmanagement.it>
2017-07-07 15:46:35 +0200

    Update SparkWordCount.scala

--- src/main/scala/com/sysman/scala/SparkWordCount.scala
+++ src/main/scala/com/sysman/scala/SparkWordCount.scala
@@ -1,3 +1,6 @@
+/** Conta le parole nel file in input (args[0] e filtra quelle il cui numero é  **/
+/** maggiore della soglia impostata in input (args[1])                          **/
+/**                               NO STREAMING                                  **/
 package com.sysman.scala
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
@@ -6,24 +9,34 @@ import org.apache.spark.SparkConf
 
 object SparkWordCount {
   def main(args: Array[String]) {
-    // create Spark context with Spark configuration
+    
+    // crea Spark context. 
     val sc = new SparkContext(new SparkConf().setAppName("Spark Count"))
 
-    // get threshold
+    // legge la soglia in input.
     val threshold = args(1).toInt
 
-    // read in text file and split each document into words
+    // legge il testo in input e lo suddivide nelle parole componenti.
+    // --> usa flatMap per creare un rdd non delimitato da \n
     val tokenized = sc.textFile(args(0)).flatMap(_.split(" "))
 
-    // count the occurrence of each word
+    // conta le occorrenze di ogni parola.
+    // --> 1) usa map per creare un rdd di coppie (parola,contatore) 
+    // --> 2) usa reduceByKey per contare le occorrenze di ogni "parola" in "contatore"
     val wordCounts = tokenized.map((_, 1)).reduceByKey(_ + _)
 
-    // filter out words with fewer than threshold occurrences
+    // filtra le parole con un numero di occorrenze minore della soglia
+    // --> usa filter per filtrare quelle coppie (parola,contatore) che hanno "contatore > soglia
     val filtered = wordCounts.filter(_._2 >= threshold)
 
-    // count characters
+    // conta i caratteri
+    // --> 1) flatMap per ridurre le occorrenze residue in un array di caratteri
+    // --> 2) map per creare coppie (carattere,contatore)
+    // --> 2) reduceByKey per contare le occorrenze di ogni carattere.
     val charCounts = filtered.flatMap(_._1.toCharArray).map((_, 1)).reduceByKey(_ + _)
-
+    
+    // Mostra a terminale i risultati.
+    // --> collect è l'action che provoca l'esecuzione di tutte le transformation inserite in precedenza.
     System.out.println(charCounts.collect().mkString(", "))
   }
 }
