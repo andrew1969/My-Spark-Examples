0e97131a3a90dbc3f24deef1f6f65f197685e4cb
andrew1969 <acarbone@sysmanagement.it>
2017-07-07 16:20:28 +0200

    Update StreamingCountKey.scala

--- src/main/scala/com/sysman/scala/StreamingCountKey.scala
+++ src/main/scala/com/sysman/scala/StreamingCountKey.scala
@@ -1,9 +1,12 @@
+/** Contenggio in Saprk Streaming delle occorrenze di un log APACHE contenenti **/
+/** lo stesso indirizzo IP. Versione StateFull                                 **/
+/** Learning Spark pp 196 - 197                                                **/
+
 package com.sysman.scala
 
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
-//import ApacheAccessLog
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.StreamingContext._
 import org.apache.spark.streaming.dstream.DStream
@@ -15,19 +18,23 @@ object StreamingCountKey {
   def main(args: Array[String]) {
     val logDirectory = "E:\\Spark\\MyEx\\FLogs"
     val outDir = "E:\\Spark\\MyEx\\outDir\\CountByState"
-    val conf = new SparkContext(new SparkConf()
-                                .setAppName("Spark Stream Log Input"))
-//                                .set("spark.local.dir", "E:\\SPARK\\MyEx\\TMP_LOCAL"))
+    val conf = new SparkContext(new SparkConf().setAppName("Spark Stream Log Input"))
     val ssc = new StreamingContext(conf, Seconds(5))
+    
+    // impostazione checkPoint
+    // --> necessaria in caso di crash/ripartenze
     ssc.checkpoint("E:\\SPARK\\MyEx\\TMP")
     val logData = ssc.textFileStream(logDirectory)
     val accessLogDStream = logData.map(line => ApacheAccessLog.parseLogLine(line))
     val responseCodeDStream = accessLogDStream.map(log => (log.getResponseCode(),1L))
+    
+    // Aggiorna la variabile di stato che conta le occorrenze di ciascun response Code tramite la funzione 
+    // di update dello stato.
     val responseCodeCountDStream = responseCodeDStream.updateStateByKey(updateRunningSum _)
     responseCodeCountDStream.saveAsTextFiles(outDir,"txt")
 
     ssc.start()
-    ssc.awaitTermination(60000)
+    ssc.awaitTermination()
 
   }
   def updateRunningSum(values: Seq[Long], state:Option[Long]) = {
